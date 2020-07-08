27e89a8aecc92a1722a35d3b8f4fc3bd8f6d18ca
cperdiansyah <chandraperdiansyah@gmail.com>
2020-07-08 22:10:53 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Submission-Dasar-Pemrograman-Web_1
+ Submission akhir kelas dasar pemrograman web dicoding
---
+++ asset/js/script.js
@@ -0,0 +1,50 @@
+window.addEventListener('scroll', function (e) {
+    //Scrolling fx nav bar
+    var nav = document.getElementById('nav');
+
+    if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
+        nav.classList.add('scroll-header');
+        nav.classList.remove('main-header');
+    } else {
+        nav.classList.add('main-header');
+        nav.classList.remove('scroll-header');
+    }
+    
+    //Scrolling fx logo & hover
+    var logo = document.getElementById('logo-id');
+
+    if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
+        logo.classList.add('logo-sroll');
+        logo.classList.remove('logo');
+    } else {
+        logo.classList.add('logo');
+        logo.classList.remove('logo-sroll');
+    }
+    
+    var header = document.getElementById('leftHeader');
+
+    if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
+        header.classList.add('left-header-scroll');
+        header.classList.remove('left-header');
+    } else {
+        header.classList.add('left-header');
+        header.classList.remove('left-header-scroll');
+    }
+
+});
+
+window.addEventListener('scroll', function (e) {
+    //Scrolling fx nav bar list
+    var list = document.getElementsByClassName('list');
+
+    for (var i = 0; i < list.length; i++) {
+        if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
+            list[i].classList.add('nav-list-scroll');
+            list[i].classList.remove('nav-list');
+        } else {
+            list[i].classList.add('nav-list');
+            list[i].classList.remove('nav-list-scroll');
+        }
+    }    
+
+});
